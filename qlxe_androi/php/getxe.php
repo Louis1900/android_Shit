dd05507cd3eda359366f15d412e2745c59fa965d
Louise <163156543+Louis1900@users.noreply.github.com>
2025-06-03 09:15:33 +0700

    getxe.php

--- qlxe_androi/php/getxe.php
+++ qlxe_androi/php/getxe.php
@@ -22,24 +22,55 @@
     //   echo json_encode($mangxe); 
 ?>
 <?php 
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
+    // header("Access-Control-Allow-Origin: *");
+    // header("Content-Type: application/json; charset=UTF-8");
 
-    $connect = mysqli_connect("localhost", "root", "", "qlxe"); 
-    mysqli_query($connect, "SET NAMES 'utf8'"); 
+    // $connect = mysqli_connect("localhost", "root", "", "qlxe"); 
+    // mysqli_query($connect, "SET NAMES 'utf8'"); 
 
-    // Lấy mã loại từ GET
-    $maloai = isset($_GET['maloai']) ? $_GET['maloai'] : '';
+    // // Lấy mã loại từ GET
+    // $maloai = isset($_GET['maloai']) ? $_GET['maloai'] : '';
 
-    // Chỉ lọc khi có mã loại
-    $query = "SELECT * FROM xe WHERE maloai = '$maloai'"; 
+    // // Chỉ lọc khi có mã loại
+    // $query = "SELECT * FROM xe WHERE maloai = '$maloai'"; 
 
-    $mangxe = array(); 
-    $data = mysqli_query($connect, $query); 
+    // $mangxe = array(); 
+    // $data = mysqli_query($connect, $query); 
 
-    while ($row = mysqli_fetch_assoc($data)) { 
-        $mangxe[] = $row; 
-    } 
+    // while ($row = mysqli_fetch_assoc($data)) { 
+    //     $mangxe[] = $row; 
+    // } 
 
-    echo json_encode($mangxe); 
+    // echo json_encode($mangxe); 
 ?>
+<?php
+$connect = mysqli_connect("localhost", "root", "", "qlxe");
+mysqli_query($connect, "SET NAMES 'utf8'");
+
+class Xe
+{
+    function __construct($maxe, $tenxe, $namsx, $maloai, $hinh)
+    {
+        $this->maxe = $maxe;
+        $this->tenxe = $tenxe;
+        $this->namsx = $namsx;
+        $this->maloai = $maloai;
+        $this->hinh = $hinh;
+    }
+}
+
+// Get maloai from the URL parameters
+$maloai = $_GET['maloai'];
+
+$query = "SELECT * FROM xe WHERE maloai = '$maloai'";
+$data = mysqli_query($connect, $query);
+
+$mangxe = array();
+while ($row = mysqli_fetch_assoc($data)) {
+    $mangxe[] = new Xe($row['maxe'], $row['tenxe'], $row['namsx'], $row['maloai'], $row['hinh']);
+}
+
+// Return JSON response
+echo json_encode($mangxe);
+?>
+
